23e37335d165a7368d9e614801ed85293ea0fd92
SandeshB <sbhetwal@sevadev.com>
2018-01-11 16:21:18 +0545

    Project setup webpack, babel, npm

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+    render(){
+        return (
+          <div>
+              <h1>Awesome React Js</h1>
+          </div>
+        );
+
+    }
+
+}
+
+render (<App/>,window.document.getElementById("app"));
